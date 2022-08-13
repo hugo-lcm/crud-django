35bf836b039d27a30ae219f9fa8dbb544a7f8815
Hugo Leonardo <hugolcm@live.com>
2022-08-13 20:35:05 -0300

    criação do model e execução das migrações

--- crud/clientes/models.py
+++ crud/clientes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cliente(models.Model):  # herança
+
+    SEXO_CHOICES = (
+        ('F', 'Feminino'),
+        ('M', 'Masculino'),
+        ('N', 'Nenhuma das opções')
+    )
+
+    nome = models.CharField(max_length=100, blank=False)  # charfield = varchar
+    data_nascimento = models.DateField(null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    profissao = models.CharField(max_length=50, null=False, blank=False)
+    sexo = models.CharField(
+        max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
+
+    def __str__(self):  # na hora de listar os clientes, retorna o nome do cliente ao invés do endereço de memória
+        return self.nome
