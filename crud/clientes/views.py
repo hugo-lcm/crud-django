14ca6d3351fc840edc408d1f608aa0da1436324b
Hugo Leonardo <hugolcm@live.com>
2022-08-13 21:23:33 -0300

    Revert "criação da view para retornar dados do cliente"
    This reverts commit 1a351e16f8b14e87498b043a25c71e9090256e93.

--- crud/clientes/views.py
+++ crud/clientes/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from .models import Cliente
 
 # Create your views here.
-
-
-def listar_clientes(request):
-    clientes = Cliente.objects.all() # faz um select na tabela Cliente e retorna todos os dados
-    return render(request, 'cliente/listar_clientes.html', {'clientes': clientes}) # render pra renderizar as infos dos clientes e entre '' o template
