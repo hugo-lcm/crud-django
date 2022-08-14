eab3161d3be7fb658269154af1acd0d7adbeae5b
Hugo Leonardo <hugolcm@live.com>
2022-08-13 21:26:05 -0300

    adição da view para retornar dados do cliente

--- crud/clientes/views.py
+++ crud/clientes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
+
+
+def listar_clientes(request):
+    # faz um select na tabela Cliente e retorna todos os dados
+    clientes = Cliente.objects.all()
+    # render pra renderizar as infos dos clientes e entre '' o template
+    return render(request, 'cliente/listar_clientes.html', {'clientes': clientes})
