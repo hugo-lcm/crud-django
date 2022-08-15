c3a25afc520c71210004e18ac1298094212a65ba
Hugo Leonardo <hugolcm@live.com>
2022-08-15 15:12:35 -0300

    configuração das urls

---
+++ crud/clientes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    # 1 caminho da url, 2 método que a rota chama, 3 nome da rota (ex no template usa o nome ao inves de http.../clientes/listar)
+    path('listar', listar_clientes, name='listar_clientes')
+]
