10173936ca731dc72bb977f8080563122f196b82
Hugo Leonardo <hugolcm@live.com>
2022-08-18 18:41:12 -0300

    criação do template

--- crud/clientes/views.py
+++ crud/clientes/views.py
@@ -8,4 +8,4 @@ def listar_clientes(request):
     # faz um select na tabela Cliente e retorna todos os dados
     clientes = Cliente.objects.all()
     # render pra renderizar as infos dos clientes e entre '' o template
-    return render(request, 'cliente/listar_clientes.html', {'clientes': clientes})
+    return render(request, 'clientes/listar_clientes.html', {'clientes': clientes})
