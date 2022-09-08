ca315684b1cc1bf26d4aa39a8596d32bd1c28f0f
Hugo Leonardo <hugolcm@live.com>
2022-09-08 19:20:30 -0300

    refatoração da views e add métodos no servicelayer

--- crud/clientes/views.py
+++ crud/clientes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import ClienteForm
 from .models import Cliente
+from .entidades import cliente
+from .services import cliente_service
 
 
 # Create your views here.
 
 
 def listar_clientes(request):
-    # faz um select na tabela Cliente e retorna todos os dados
-    clientes = Cliente.objects.all()
+    clientes = cliente_service.listar_cliente()
     # render pra renderizar as infos dos clientes e entre '' o template
     return render(request, 'clientes/listar_clientes.html', {'clientes': clientes})
 
@@ -17,7 +18,14 @@ def inserir_cliente(request):
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
-            form.save()
+            nome = form.cleaned_data['nome']
+            sexo = form.cleaned_data['sexo']
+            data_nascimento = form.cleaned_data['data_nascimento']
+            email = form.cleaned_data['email']
+            profissao = form.cleaned_data['profissao']
+            cliente_novo = cliente.Cliente(nome=nome, sexo=sexo, data_nascimento=data_nascimento,
+                                           email=email, profissao=profissao)
+            cliente_service.cadastrar_cliente(cliente_novo)
             return redirect('listar_clientes')
     else:
         form = ClienteForm()
@@ -26,22 +34,29 @@ def inserir_cliente(request):
 
 
 def listar_cliente_id(request, id):
-    cliente = Cliente.objects.get(id=id)  # busca na tabela pelo id passado como parâmetro
+    cliente = cliente_service.listar_cliente_id(id)
     return render(request, 'clientes/listar_cliente.html', {'cliente': cliente})
 
 
 def editar_cliente(request, id):
-    cliente = Cliente.objects.get(id=id)
-    form = ClienteForm(request.POST or None, instance=cliente)
+    cliente_antigo = cliente_service.listar_cliente_id(id)
+    form = ClienteForm(request.POST or None, instance=cliente_antigo)
     if form.is_valid():
-        form.save()
+        nome = form.cleaned_data['nome']
+        sexo = form.cleaned_data['sexo']
+        data_nascimento = form.cleaned_data['data_nascimento']
+        email = form.cleaned_data['email']
+        profissao = form.cleaned_data['profissao']
+        cliente_novo = cliente.Cliente(nome=nome, sexo=sexo, data_nascimento=data_nascimento,
+                                       email=email, profissao=profissao)
+        cliente_service.editar_cliente(cliente_antigo, cliente_novo)
         return redirect('listar_clientes')
     return render(request, 'clientes/form_cliente.html', {'form': form})
 
 
 def remover_cliente(request, id):
-    cliente = Cliente.objects.get(id=id)
+    cliente = cliente_service.listar_cliente_id(id)
     if request.method == 'POST':
-        cliente.delete()
+        cliente_service.remover_cliente(cliente)
         return redirect('listar_clientes')
     return render(request, 'clientes/confirma_exclusao.html', {'cliente': cliente})
--- crud/crud/urls.py
+++ crud/crud/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('clientes/', include('clientes.urls'))
+    # path('admin/', admin.site.urls),
+    path('', include('clientes.urls'))
 ]
